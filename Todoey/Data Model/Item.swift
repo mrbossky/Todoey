76b61d9f95b429673d4a40318d5af8894cd715aa
mrbossky <mrbossky@gmail.com>
2018-07-17 11:50:20 -0700

    Done Project

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -2,13 +2,16 @@
 //  Item.swift
 //  Todoey
 //
-//  Created by Viktor Tarasov on 7/17/18.
-//  Copyright © 2018 Viktor Tarasov. All rights reserved.
+//  Created by Angela Yu on 12/12/2017.
+//  Copyright © 2017 Angela Yu. All rights reserved.
 //
 
 import Foundation
+import RealmSwift
 
-class Item {
-    var title : String = ""
-    var done : Bool = false
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
